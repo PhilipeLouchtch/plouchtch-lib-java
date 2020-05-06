f80ce087cd15855d5cdd49e9705094a43cbc216f
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2020-05-06 17:19:49 +0200

    add the 'ImplementMe' exception

---
+++ src/main/java/plouchtch/lang/exception/ImplementMe.java
@@ -0,0 +1,9 @@
+package plouchtch.lang.exception;
+
+public class ImplementMe extends RuntimeException
+{
+	public ImplementMe()
+	{
+		super("Functionality not implemented. Now is probably the time to do so, even if it might not be the best of times :/ ?");
+	}
+}
